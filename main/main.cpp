905fd744f9160b6d5f28a12dd9c99c72392de086
OlegNotFound38 <pronkinoa@gmail.com>
2026-06-24 20:48:55 +0300

    заготовка для вывода эмблемы

--- main/main.cpp
+++ main/main.cpp
@@ -8,5 +8,21 @@ using namespace std;
 
 int main(){
 
+    cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << endl
+         << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" << endl
+         << "-|                               |-" << endl;
+    cout << "|-    V            CCCCVC        -|" << endl
+         << "-|     V         CC  V   CC      |-" << endl
+         << "|-      V       CC  V      C     -|" << endl
+         << "-|       V     C   V             |-" << endl
+         << "|-        V    C  V              -|" << endl
+         << "-|         V   C V               |-" << endl
+         << "|-          V   VC         C     -|" << endl
+         << "-|           V V CC      CC      |-" << endl
+         << "|-            V    CCCCCC        -|" << endl;
+    cout << "-|                               |-" << endl
+         
+         
+
     return 0;
 }
