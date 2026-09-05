61c2baae40c68fae2884b529f4925903ceca53ac
OlegNotFound38 <pronkinoa@gmail.com>
2026-09-05 22:41:38 +0300

    trimVideo interface

--- main/main.cpp
+++ main/main.cpp
@@ -15,10 +15,31 @@ void sleep(const int milliseconds){
         this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
 }
 
+void trim_video(){
+    cout << endl;
+    for (short i = 0; i < 4; i++){
+        cout << ". ";
+        sleep(150); 
+    }
+    cout << endl;
+
+    cout << "1. Trim the beginning" << endl;
+    sleep(80);
+    cout << "2. Trim the end" << endl;
+    sleep (120);
+    cout << "3. Back to main menu";
+
+    short u_cb; cin >> u_cb;
+    if (u_cb == 1) u_cb = 1; //trim_begin();
+    else if (u_cb == 2) u_cb = 2; //trim_end();
+    else if (u_cb == 3) main_menu();     
+}
+
 
 void main_menu(){
-    cout << "1. Trim a video" << endl
-         << "2. Quit" << endl;
+    cout << "1. Trim a video" << endl;
+    sleep(80);
+    cout << "2. Quit" << endl;
 
     short user_callback; cin >> user_callback;
     if (user_callback == 1) trim_video();
